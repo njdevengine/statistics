a0a6db8bf03ac033c71082a5e6ee80090cadb691
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-09-16 02:57:56 -0400

    Update probability.py

--- probability.py
+++ probability.py
@@ -12,3 +12,21 @@ def prb_a_and_b(prf_a,pos_a,prf_b,pos_b):
     return(prob_a*prob_b)
     
 prb_a_and_b(1,13,1,4)
+
+#experimental probabilities
+
+def exp_probability(successful,total):
+    return(successful/total)
+
+exp_probability(8,10)
+
+#probability of an event (drawing a spade) 
+#after drawing a card 20 times (20 trials)
+#theoretical probability E(A) = P(A)xn
+
+def exp_probability_2(prf_a,pos_a,trials):
+    prob_a = probability(prf_a,pos_a)
+    exp = prob_a*trials
+    return(exp)
+
+exp_probability_2(1,4,20)
